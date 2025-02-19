d99d53c0be048238f1e0af92929e6fc2c88e9f09
Abdul Qadir <abdulqadir116869@gmail.com>
2025-02-19 03:43:25 -0800

    Initial commit

---
+++ JavaScript/js variables/index.js
@@ -0,0 +1,21 @@
+
+//  let > redeclared is not working 
+//  let > change are working
+//  let > are block scop
+let a = 2
+let b = 6
+console.log(a + b)  // ans > 8
+
+//  const > redeclared is  working 
+//  const > change is  working working
+//  const > are block scop
+const qasim = 20
+const rehman = 10
+console.log(qasim-rehman)  // ans > 10
+
+//  const > redeclared is not working 
+//  const > change is not working working
+//  const > are global scop
+var ali = 3
+var sana = 4
+console.log(ali*sana)  // ans > 12
